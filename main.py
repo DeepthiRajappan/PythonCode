cfd7b4d6b14a13b8f62ca1513f3485b11b4a1e16
DeepthiRajappan <46626455+DeepthiRajappan@users.noreply.github.com>
2019-01-13 13:54:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 13 13:48:13 2019
+
+@author: Deepthi Prakash
+"""
+
+""" Details about my favourite song
+This song is from the film 96.   """
+
+NameOfSong="Kaathale Kaathale"
+Artist="Chinmayi"
+Genre="Romance"
+SongDuration=3.13
+AlbumName=96
+MusicDirector="Govind Vasantha"
+YearOfRelease=2018
+ProductionHouse="Think Music"
+FeaturedArtists="Trisha and Vijay Sethupathi"
+print("Name of the song:",NameOfSong)  #prints the name of the song
+print("Artist:",Artist) #prints the performing artist
+print("Genre:",Genre) #prints the genre of the song
+print("Duration:",SongDuration) #prints the song duration
+print("Name of Album:",AlbumName) #prints the album name
+print("Name of Music Director:",MusicDirector) #Prints the name of the music director 
+print("Release Date:",YearOfRelease) #prints the year of release
+print("Production House:",ProductionHouse) #prints the label of the production house
+print("Featured Artists:",FeaturedArtists) #prints the name of the artists in the featured video of the song, If any.
+
